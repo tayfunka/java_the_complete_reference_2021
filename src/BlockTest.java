3f1f455c356d376300d3ea67e185dc2a0105924d
tayfunkarakavuz <karakavuz@proxycode.jp>
2025-02-08 17:15:51 +0300

    ch1, ch2

--- src/BlockTest.java
+++ src/BlockTest.java
@@ -4,8 +4,8 @@ class BlockTest {
 
     y = 20;
     for (x = 0; x < 20; x++) {
-        System.out.println("This is x: " + x);
-        System.out.println("This is y: " + y);
+        System.out.println("This is  x: " + x);
+        System.out.println("This is  y: " + y);
         y = y - 2;
         }
     }
