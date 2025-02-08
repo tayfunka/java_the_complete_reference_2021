fdf00a5678a2ca8ce5f54cc63a21c300aedbcbc1
tayfunkarakavuz <karakavuz@proxycode.jp>
2025-02-08 17:14:04 +0300

    ch1, ch2

---
+++ src/Example.java
@@ -0,0 +1,9 @@
+/*
+Call this file Example.java
+*/
+
+class Example {
+    public static void main(String [] args) {
+        System.out.println("This is an Example java program");
+    }
+}
---
+++ src/ForTest.java
@@ -0,0 +1,7 @@
+class ForTest {
+    public static void main(String[] args) {
+        int x;
+        for (x = 0; x < 10; x++)
+            System.out.println(x);
+    }
+}
